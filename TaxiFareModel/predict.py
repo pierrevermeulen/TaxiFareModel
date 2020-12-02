4d55421a267a54e6449b6b841897a0ebd2ab6838
Pierre Vermeulen <pierre.vermeulen@espci.org>
2020-12-02 21:23:14 +0100

    Complete predict.py

--- TaxiFareModel/predict.py
+++ TaxiFareModel/predict.py
@@ -3,34 +3,29 @@ from math import sqrt
 
 import joblib
 import pandas as pd
+#from TaxiFareModel.params import MODEL_NAME
 from google.cloud import storage
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-BUCKET_NAME = ''
-PATH_INSIDE_BUCKET = ""
-MODEL_NAME = ""
-
+BUCKET_NAME = "wagon-bootcamp-1-297409"
+BUCKET_TRAIN_DATA_PATH = "data/train_1k.csv"
+MODEL_NAME = "taxifare_v2_xgboost.joblib"
 
 def get_test_data():
     """method to get the training data (or a portion of it) from google cloud bucket
     To predict we can either obtain predictions from train data or from test data"""
     # Add Client() here
     client = storage.Client()
-    # if local:
-    #     path = "data/data_data_10Mill.csv"
-    # else:
-    ######path = "gs://wagon-ml-galicier-taxifare/data/test.csv"
-    df = pd.read_csv(path)
+    df = pd.read_csv("gs://{}/{}".format(BUCKET_NAME, BUCKET_TRAIN_DATA_PATH), nrows=1000)
+
     return df
 
 
-def download_model(model_directory="PipelineTest", bucket=BUCKET_NAME, rm=True):
+def download_model(bucket=BUCKET_NAME, rm=True):
     client = storage.Client().bucket(bucket)
 
-    storage_location = 'models/{}/{}/{}'.format(
-        MODEL_NAME,
-        model_directory,
-        'model.joblib')
+    storage_location = 'models/{}'.format(MODEL_NAME)
+    print(storage_location)
     blob = client.blob(storage_location)
     blob.download_to_filename('model.joblib')
     print(f"=> pipeline downloaded from storage")
@@ -49,18 +44,28 @@ def evaluate_model(y, y_pred):
 
 def generate_submission_csv(folder="Pipeline", kaggle_upload=False):
     df_test = get_test_data()
-    pipeline = download_model(folder)
-    # Check if model savec was the ouptut of RandomSearch or Gridsearch
+    X_test = df_test.drop("fare_amount", axis =1)
+    y_test = None
+    if "fare_amount" in df_test.columns:
+        y_test = df_test["fare_amount"]
+    pipeline = download_model()
+    # Check if model saved was the ouptut of RandomSearch or Gridsearch
     if "best_estimator_" in dir(pipeline):
-        y_pred = pipeline.best_estimator_.predict(df_test)
+        y_pred = pipeline.best_estimator_.predict(X_test)
     else:
-        y_pred = pipeline.predict(df_test)
+        y_pred = pipeline.predict(X_test)
+    if isinstance(y_test, pd.Series):
+        res = evaluate_model(y_test, y_pred)
+        for k, v in res.items():
+            print(f"Metric {k}: value {v:.2f}")
+
     df_test["fare_amount"] = y_pred
     df_sample = df_test[["key", "fare_amount"]]
-    name = f"predictions_{folder}.csv"
-    df_sample.to_csv(name, index=False)
-    print(res)
+
     if kaggle_upload:
+        name = f"predictions_{folder}.csv"
+        df_sample.to_csv(name, index=False)
+        print("prediction saved under kaggle format")
         kaggle_message_submission = name[:-4]
         command = f'kaggle competitions submit -c new-york-city-taxi-fare-prediction -f {name} -m "{kaggle_message_submission}"'
         os.system(command)
@@ -69,6 +74,4 @@ def generate_submission_csv(folder="Pipeline", kaggle_upload=False):
 if __name__ == '__main__':
     folder = "Pipeline"
     # model = download_model(folder)
-    generate_submission_csv(folder, kaggle_upload=True)
-
-
+    generate_submission_csv(folder, kaggle_upload=False)
